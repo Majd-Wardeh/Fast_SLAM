b3d39361465e59e4f5ca2eb5b01cd2ecb0e2f590
Majd Wardeh <majd.wardeh1@gmail.com>
2020-12-15 17:42:28 +0200

    A_start finished with debug

--- script/A_star.py
+++ script/A_star.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import cv2
 import numpy as np
 from numpy import pi, cos, sin
@@ -7,7 +8,7 @@ from math import sqrt
 from Queue import PriorityQueue
 
 class Node:
-    width = 211
+    width = 211    
     def __init__(self, point, parent, G_value, H_value):
         self.point = point
         self.parent = parent
@@ -29,6 +30,10 @@ class Graph:
         self.start_point = start_point
         self.goal_point = goal_point
         self.h, self.w = image.shape
+        self.debug_image = np.zeros((self.h, self.w, 3), dtype=np.uint8)
+        self.debug_image[:, :, 0] = self.image
+        self.debug_image[start_point[0], start_point[1], 2] = 255
+        self.debug_image[goal_point[0], goal_point[1], 2] = 255
 
         assert self.feasible(start_point), 'the start point is not feasible.'
         assert self.feasible(goal_point), 'the start point is not feasible.'
@@ -41,29 +46,43 @@ class Graph:
     def find_path(self):
         while not self.openList.empty():
             node = self.openList.get()
+            if node.id in self.closeSet:
+                continue
             self.closeSet.add(node.id)
-            print(node)
-            print(node.id)
-            print(self.closeSet)
-            _ = raw_input('...')
+
+            self.debug_image[node.point[0], node.point[1], 1] = 255
+            cv2.imshow('debug image', self.debug_image)
+            cv2.waitKey(1)
+
+            print('{} and node.Id = {}'.format(node, node.id))
+
             if (node.point == self.goal_point).all():
                 print('we have reached the goal!')
                 print('the distance is ', node.G)
+                break
             else:
                 self.update_openList(node)
+
+        self.debug_image[:, :, 1] = np.uint8(self.debug_image[:, :, 1]*0.3)
+        while node != None:
+            self.debug_image[node.point[0], node.point[1], 2] = 255
+            node = node.parent
+        cv2.imshow('debug image', self.debug_image)
+        cv2.waitKey(0)   
             
     def dis(self, S, G):
         return la.norm(S-G)
 
     def update_openList(self, parentNode):
+        print('pnode: {} had neighborhoods: '.format(parentNode), end='')
         x, y = parentNode.point
         points = np.array([[x+1, y], [x-1, y], [x, y+1], [x, y-1], [x+1, y+1], [x-1, y-1], [x-1, y+1], [x+1, y-1]])
         for p in points:
             if p[0] < 0 or p[0] >= self.w or p[1] < 0 or p[1] > self.h:
-                print('outside the array bounds', p)
+                # print('outside the array bounds', p)
                 continue
             if self.image[p[0], p[1]] > 0:
-                print('obstacle p', self.image[p[0], p[1]])
+                # print('obstacle p', self.image[p[0], p[1]])
                 continue
             p_id = self.calc_id(p)
             if p_id not in self.closeSet:
@@ -71,6 +90,8 @@ class Graph:
                 G = parentNode.G + self.dis(parentNode.point, p)
                 node = Node(p, parentNode, G, H)
                 self.openList.put(node)
+                print('{}, '.format(node), end='')
+        print()
 
     def feasible(self, p):
         condition1 = p[0] < 0 or p[0] >= self.w or p[1] < 0 or p[1] > self.h
@@ -98,10 +119,15 @@ kernel2 = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1] ], dtype=np.uint8)
 
 # updated_image1 = cv2.dilate(image, kernel1, iterations=5)
 img = cv2.dilate(image, kernel2, iterations=5)
-
-
 S = np.array([80, 180])
 G = np.array([150, 50])
+
+
+#debug
+# img = np.zeros((10, 10), dtype=np.uint8)
+# S = np.array([0, 0])
+# G = np.array([9, 9])
+
 
 graph = Graph(img, S, G)
 
